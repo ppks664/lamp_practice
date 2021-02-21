04b058059d9822fb664763a0d0877796637a87b1
ren <kureigu007bondo@outlook.jp>
2021-02-22 08:28:20 +0900

    並び替え

--- www/html/index.php
+++ www/html/index.php
@@ -9,11 +9,18 @@ session_start();
 if(is_logined() === false){
   redirect_to(LOGIN_URL);
 }
+
+//get通信で送信された選択した値を取得する
+$change = get_get('change');
+//get_open_items
+if($change === ''){
+  $change = '新着順';
+}
 //db接続
 $db = get_db_connect();
 //ユーザーデータベースに接続
 $user = get_login_user($db);
 //商品のデータベースに接続
-$items = get_open_items($db);
+$items = get_open_items($db,$change);
 $token = get_csrf_token();
 include_once VIEW_PATH . 'index_view.php';
